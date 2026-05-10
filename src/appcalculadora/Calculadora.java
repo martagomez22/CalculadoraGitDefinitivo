fa3e911ab62da0d302584ad83664cbf4a228e0ba
Marti <Marti@Marta>
2026-05-10 04:43:42 +0200

    Proyecto Base

---
+++ src/appcalculadora/Calculadora.java
@@ -0,0 +1,12 @@
+package appcalculadora;
+
+public class Calculadora {
+
+    private int num1;
+    private int num2;
+
+    public Calculadora(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+}
